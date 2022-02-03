5ed4f4198f9d20aa28f7b68b59ac7ec8e85d35eb
Jerome Eteve <jerome@eteve.net>
2022-02-03 13:27:39 +0000

    Changed candidates to enable scoring

--- src/solver.rs
+++ src/solver.rs
@@ -1,16 +1,61 @@
 use lazy_static::lazy_static;
+//use std::borrow::Borrow;
 use std::cell::RefCell;
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeSet, HashMap, HashSet};
 use std::error;
 use std::fmt;
+use std::hash::Hash;
 
 lazy_static! {
     static ref EMPTYSET: HashSet<&'static String> = HashSet::new();
 }
 
+struct ScoredString<'a> {
+    s: &'a String,
+    score: f64,
+}
+
+// See https://stackoverflow.com/questions/70978841/lifetime-in-mutable-structure-with-hashset/70979198#70979198
+// impl Borrow<str> for ScoredString<'_>{
+//     fn borrow(&self) -> &str {
+//         self.s
+//     }
+// }
+
+impl<'a> ScoredString<'a> {
+    fn new(s: &'a String, score: f64) -> ScoredString<'a> {
+        ScoredString { s, score }
+    }
+}
+impl Eq for ScoredString<'_> {}
+impl Hash for ScoredString<'_> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.s.hash(state);
+    }
+}
+impl PartialEq for ScoredString<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.s == other.s
+    }
+}
+impl PartialOrd for ScoredString<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+impl Ord for ScoredString<'_> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.score > other.score {
+            std::cmp::Ordering::Greater
+        } else {
+            std::cmp::Ordering::Less
+        }
+    }
+}
+
 /// A wordle solver build from a dictionary
 pub struct Solver<'a> {
-    candidates: RefCell<HashSet<&'a String>>,
+    candidates: RefCell<BTreeSet<ScoredString<'a>>>,
     exists_letters: RefCell<HashSet<char>>,
     by_letter: HashMap<char, HashSet<&'a String>>,
     by_letter_position: HashMap<(char, usize), HashSet<&'a String>>,
@@ -68,7 +113,20 @@ impl<'a> Solver<'a> {
     ///  let solver = Solver::new(&dictionary);
     /// ```
     pub fn new(dictionary: &HashSet<String>) -> Solver {
-        let candidates = dictionary.iter().collect::<HashSet<&String>>();
+        let _letter_freq =
+            dictionary
+                .iter()
+                .flat_map(|w| w.chars())
+                .fold(HashMap::new(), |mut m, c| {
+                    *m.entry(c).or_insert(0) += 1;
+                    m
+                });
+
+        let candidates = dictionary
+            .iter()
+            .map(|s| ScoredString::new(s, 0.0))
+            .collect();
+
         let by_letter = dictionary.iter().fold(HashMap::new(), |mut h, v| {
             for c in v.chars() {
                 let entry = h.entry(c).or_insert_with(HashSet::new);
@@ -105,7 +163,7 @@ impl<'a> Solver<'a> {
     ///
     /// If you give inconsistent hints, this might return `none`.
     pub fn first_candidate(&self) -> Option<&String> {
-        return self.candidates.borrow().iter().next().copied();
+        return self.candidates.borrow().iter().next().map(|v| v.s);
     }
 
     fn with_letter(&self, l: &char) -> &HashSet<&String> {
@@ -146,18 +204,12 @@ impl<'a> Solver<'a> {
         })
     }
 
-    /// Some words might be in your dictionary but not
-    /// in wordle. Use this to discard them
-    pub fn discard_word(&self, s: &str) {
-        self.candidates.borrow_mut().remove(&s.to_string());
-    }
-
     fn add_well_placed(&self, l: &char, p: &usize) {
         //println!("Restricting to words containing an {} at position {}" , l, p);
         let to_retain = self.with_letter_in_position(l, p).clone();
         self.candidates
             .borrow_mut()
-            .retain(|s| to_retain.contains(s));
+            .retain(|s| to_retain.contains(s.s));
         self.exists_letters.borrow_mut().insert(*l);
     }
 
@@ -167,13 +219,13 @@ impl<'a> Solver<'a> {
         let to_retain = self.with_letter(l).clone();
         self.candidates
             .borrow_mut()
-            .retain(|s| to_retain.contains(s));
+            .retain(|s| to_retain.contains(s.s));
 
         //println!("Removing words with an {} at position {}" , l, p);
         let to_remove = self.with_letter_in_position(l, p).clone();
         self.candidates
             .borrow_mut()
-            .retain(|s| !to_remove.contains(s));
+            .retain(|s| !to_remove.contains(s.s));
         self.exists_letters.borrow_mut().insert(*l);
     }
 
@@ -188,8 +240,17 @@ impl<'a> Solver<'a> {
             let to_remove = self.with_letter(l).clone();
             self.candidates
                 .borrow_mut()
-                .retain(|s| !to_remove.contains(s));
+                .retain(|s| !to_remove.contains(s.s));
         }
+    }
+}
+
+impl<'a> Solver<'a> {
+    /// Some words might be in your dictionary but not
+    /// in wordle. Use this to discard them
+    pub fn discard_word(&self, s: &str) {
+        let mut candidates = self.candidates.borrow_mut();
+        candidates.retain(|ss| !ss.s.eq(s))
     }
 }
 
